677f536ae3463ab92c8ec2809e62c2c0a9fe5bc6
Himanshu Warekar <himanshuwarekar@Himanshus-MacBook-Air.local>
2018-10-05 15:37:53 +0530

    audits

--- quality_management/quality_management/doctype/audit_type/audit_type.js
+++ quality_management/quality_management/doctype/audit_type/audit_type.js
@@ -1,11 +1,8 @@
 // Copyright (c) 2018, Frappe and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Quality Review', {
+frappe.ui.form.on('Audit Type', {
 	refresh: function(frm) {
 
-	},
-	procedure: function(frm) {
-		frm.set_value("date", frappe.datetime.get_today())
 	}
 });
