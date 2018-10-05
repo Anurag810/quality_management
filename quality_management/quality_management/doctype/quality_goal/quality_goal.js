783d33599bafc837adabc74cd73694afdf07bd91
Himanshu Warekar <himanshuwarekar@Himanshus-MacBook-Air.local>
2018-10-05 15:22:17 +0530

    dynamic dates

--- quality_management/quality_management/doctype/quality_goal/quality_goal.js
+++ quality_management/quality_management/doctype/quality_goal/quality_goal.js
@@ -8,19 +8,10 @@ frappe.ui.form.on('Quality Goal', {
 	revision: function(frm) {
 		frm.set_value("revised_on", frappe.datetime.get_today())
 	},
-	scope: function(frm) {
-		//	frm.set_value("revised_on", get_today())
-	}
 });
 
 frappe.ui.form.on('Quality Objective', {
-	frequency(frm, cdt, cdn) {
-		console.log(frm.fields_dict)
-		objective = frappe.get_doc(cdt, cdn)
-		// console.log(objective)
-		// if objective.frequency == "Weekly":
-		// 	objective.
-		// if objective.frequency == "Weekly":
-		// objective.
+	frequency: function(frm, cdt, cdn) {
+		console.log(cdt, cdn);
 	}
 });
