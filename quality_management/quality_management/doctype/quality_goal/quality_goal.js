a2fcbf642bfacef8fe8f25adfcd732c6a91f208f
Himanshu Warekar <himanshuwarekar@Himanshus-MacBook-Air.local>
2018-10-05 14:45:24 +0530

    auto update date

--- quality_management/quality_management/doctype/quality_goal/quality_goal.js
+++ quality_management/quality_management/doctype/quality_goal/quality_goal.js
@@ -4,5 +4,23 @@
 frappe.ui.form.on('Quality Goal', {
 	refresh: function(frm) {
 
+	},
+	revision: function(frm) {
+		frm.set_value("revised_on", frappe.datetime.get_today())
+	},
+	scope: function(frm) {
+		//	frm.set_value("revised_on", get_today())
+	}
+});
+
+frappe.ui.form.on('Quality Objective', {
+	frequency(frm, cdt, cdn) {
+		console.log(frm.fields_dict)
+		objective = frappe.get_doc(cdt, cdn)
+		// console.log(objective)
+		// if objective.frequency == "Weekly":
+		// 	objective.
+		// if objective.frequency == "Weekly":
+		// objective.
 	}
 });
