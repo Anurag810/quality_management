ab5e8a4d5c65010d92b43741e271bc601e583f35
Himanshu Warekar <himanshuwarekar@Himanshus-MacBook-Air.local>
2018-10-06 20:12:43 +0530

    dynamic monitoring frequencies

--- quality_management/quality_management/doctype/quality_monitoring_frequency/quality_monitoring_frequency.js
+++ quality_management/quality_management/doctype/quality_monitoring_frequency/quality_monitoring_frequency.js
@@ -4,5 +4,23 @@
 frappe.ui.form.on('Quality Monitoring Frequency', {
 	refresh: function(frm) {
 
+	},
+	frequency: function(frm){
+		var frequency = frm.get_field("frequency").value;
+		if (frequency == "Weekly"){
+			frm.set_df_property("day", "options", "A\nB\nC");
+		}
+		else if (frequency == "Monthly"){
+
+		}
+		else if (frequency == "Quarterly"){
+
+		}
+		else if (frequency == "Half-Yearly"){
+
+		}
+		else{
+
+		}
 	}
 });
