9cc83bfc02623e4d5ed749f16a38c66dbd05290a
Himanshu Warekar <himanshuwarekar@Himanshus-MacBook-Air.local>
2018-10-07 11:18:09 +0530

    frequency changes

--- quality_management/quality_management/doctype/quality_monitoring_frequency/quality_monitoring_frequency.js
+++ quality_management/quality_management/doctype/quality_monitoring_frequency/quality_monitoring_frequency.js
@@ -17,13 +17,13 @@ frappe.ui.form.on('Quality Monitoring Frequency', {
 			frm.set_df_property("day", "options", "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n26\n27\n28\n29\n30\n31");
 		}
 		else if (frequency == "Quarterly"){
-			frm.set_df_property("day", "options", "First Quarter\nSecond Quarter\nThird Quarter\nFourth Quarter");
+			frm.set_df_property("day", "options", "First Quarter-January 1\nSecond Quarter-April 1\nThird Quarter-July 1\nFourth Quarter-October 1");
 		}
 		else if (frequency == "Half Yearly"){
-			frm.set_df_property("day", "options", "First Half\nSecond Half");
+			frm.set_df_property("day", "options", "First Half-January 1\nSecond Half-July 1");
 		}
 		else{
-
+			frm.set_df_property("day", "options", "Year-January 1");
 		}
 	}
 });
