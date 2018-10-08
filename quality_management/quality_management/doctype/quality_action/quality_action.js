2ed2bf4d21b2c165d081917bc73e412d74323729
Himanshu Warekar <himanshuwarekar@Himanshus-MacBook-Air.local>
2018-10-08 16:47:27 +0530

    dynamic changes

--- quality_management/quality_management/doctype/quality_action/quality_action.js
+++ quality_management/quality_management/doctype/quality_action/quality_action.js
@@ -9,12 +9,22 @@ frappe.ui.form.on('Quality Action', {
 		frm.set_value("date", frappe.datetime.get_today())
 	},
 	goal: function(frm){
-		frm.set_query("objective", {
-			"filters": {
-				"parenttype": "Quality Goal",
-				"parentfield": "objective",
-				"parent": frm.doc.goal
-			}
-		});
+		frappe.call({
+            "method": "frappe.client.get",
+            args: {
+                doctype: "Quality Goal",
+				name: frm.doc.goal
+            },
+            callback: function (data) {
+				var string = "";
+				for (var i = 0; i < data.message.objective.length; i++ ){
+					string = string + data.message.objective[i].objective;
+					if (i != data.message.objective.length-1 ){
+						string = string + "\n";	
+					}
+				}
+				frm.set_df_property("objective", "options", string)
+            }
+        })
 	}
 });
