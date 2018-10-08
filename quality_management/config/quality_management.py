b0fac04d6e04ba12432f70744d3dc69d34e4dfa4
Anurag mishra <anuragmishra@Anurags-MacBook-Air.local>
2018-10-08 16:58:21 +0530

    change

---
+++ quality_management/config/quality_management.py
@@ -0,0 +1,52 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "label": "QMS1",
+            "items": [
+                {
+                    "type": "doctype",
+                    "name": "Quality Goal"
+                }
+            ]
+        },
+        {
+            "label": "QMS2",
+            "items": [
+                {
+                    "type": "doctype",
+                    "name": "Quality Review"
+                },
+                {
+                    "type": "doctype",
+                    "name": "Quality Procedure"
+                },
+                {
+                    "type": "doctype",
+                    "name": "Customer Feedback"
+
+                },
+                {
+                    "type": "doctype",
+                    "name": "Quality Action"
+
+                },
+                {
+                    "type": "doctype",
+                    "name": "Quality Audit"
+{
+                    "type": "doctype",
+                    "name": "Quality Audit Type"
+
+                }
+                },
+                {
+                    "type": "doctype",
+                    "name": "Quality Monitoring Frequency"
+
+                }
+            ]
+        }
+    ]
