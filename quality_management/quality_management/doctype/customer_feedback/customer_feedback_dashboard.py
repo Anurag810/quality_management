a98aa2d4ea5478bef41c8b02d5eb937880e35a77
Himanshu Warekar <himanshuwarekar@Himanshus-MacBook-Air.local>
2018-10-08 22:19:34 +0530

    changes

--- quality_management/quality_management/doctype/customer_feedback/customer_feedback_dashboard.py
+++ quality_management/quality_management/doctype/customer_feedback/customer_feedback_dashboard.py
@@ -2,11 +2,11 @@ from frappe import _
 
 def get_data():
     return {
-        'fieldname': 'customer_feedback',
+        'fieldname': '',
         'transactions': [
             {
                 'label': _('Dummy'),
-                'items': ['Quality Action', 'Quality Review']
+                'items': ['']
             }
         ],
     }
