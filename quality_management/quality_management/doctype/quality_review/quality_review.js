5b1cc3b381129c7519241dfb681b2be3973d1be7
Himanshu Warekar <himanshuwarekar@Himanshus-MacBook-Air.local>
2018-10-09 15:29:27 +0530

    changes

--- quality_management/quality_management/doctype/quality_review/quality_review.js
+++ quality_management/quality_management/doctype/quality_review/quality_review.js
@@ -5,7 +5,7 @@ frappe.ui.form.on('Quality Review', {
 	refresh: function(frm) {
 
 	},
-	procedure: function(frm) {
+	goal: function(frm) {
 		frm.set_value("date", frappe.datetime.get_today())
 		frappe.call({
             "method": "frappe.client.get",
@@ -14,31 +14,16 @@ frappe.ui.form.on('Quality Review', {
 				name: frm.doc.goal
             },
             callback: function (data) {
-				var string = "";
-				for (var i = 0; i < data.message.objective.length; i++ ){
-					string = string + data.message.objective[i].objective;
-					if (i != data.message.objective.length-1 ){
-						string = string + "\n";	
-					}
-				}
-				frm.set_df_property("objective", "options", string)
+				console.log(data.message.objective[0].objective);
+				//var string = "";
+				//for (var i = 0; i < data.message.objective.length; i++ ){
+				//	string = string + data.message.objective[i].objective;
+				//	if (i != data.message.objective.length-1 ){
+				//		string = string + "\n";	
+				//	}
+				//}
+				frm.set_df_property("objective", data.message.objective[0].objective);
             }
         })
 	},
-	objective: function(frm) { 
-		frappe.call({
-            "method": "frappe.client.get",
-            args: {
-                doctype: "Quality Goal",
-				name: frm.doc.goal
-            },
-            callback: function (data) {
-				for (var i = 0; i < data.message.objective.length; i++ ){
-					if (data.message.objective[i].objective == frm.doc.objective){
-						frm.set_value("target", data.message.objective[i].target);
-					}
-				}
-			}
-        })
-	}
 });
