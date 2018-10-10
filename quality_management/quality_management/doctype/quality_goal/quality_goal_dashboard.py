8c468428d90b2692e5233ead1040aa294de98d0f
Himanshu Warekar <himanshuwarekar@Himanshus-MacBook-Air.local>
2018-10-10 11:33:32 +0530

    changes

--- quality_management/quality_management/doctype/quality_goal/quality_goal_dashboard.py
+++ quality_management/quality_management/doctype/quality_goal/quality_goal_dashboard.py
@@ -6,7 +6,7 @@ def get_data():
         'transactions': [
             {
                 'label': _('Dummy'),
-                'items': ['Quality Review', 'Quality Action', 'Quality Procedure']
+                'items': ['Quality Review', 'Quality Action']
             }
         ],
     }
