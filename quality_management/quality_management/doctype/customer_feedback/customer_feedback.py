9fdc87d6a28bbdfb1ff70ccf74bb3e1c132c026d
Anurag mishra <anuragmishra@Anurags-MacBook-Air.local>
2018-10-11 11:59:11 +0530

    changed costumer feedback backend

--- quality_management/quality_management/doctype/customer_feedback/customer_feedback.py
+++ quality_management/quality_management/doctype/customer_feedback/customer_feedback.py
@@ -8,14 +8,18 @@ from frappe.model.document import Document
 
 class CustomerFeedback(Document):
 	
-	def after_insert(self):
-		doc = frappe.get_doc({
-			'doctype': 'Quality Action',
-			'action': 'Corrective',
-			'type': 'Customer Feedback',
-			'feedback': ''+ self.name +'',
-			'date': ''+ frappe.utils.nowdate() +'',
-			'problem': ''+ self.description +''
-		})
-		doc.insert()
-		doc.name
+	def on_update(self):
+		query = frappe.db.sql("""SELECT * FROM `tabQuality Action` WHERE feedback='"""+self.name+"""'""", as_dict=1)
+		if len(query) == 0:
+			doc = frappe.get_doc({
+				'doctype': 'Quality Action',
+				'action': 'Corrective',
+				'type': 'Customer Feedback',
+				'feedback': ''+ self.name +'',
+				'date': ''+ frappe.utils.nowdate() +'',
+				'problem': ''+ self.description +''
+			})
+			doc.insert()
+			doc.name
+		else:
+			query = frappe.db.sql("""UPDATE `tabQuality Action` SET problem='"""+ self.description +"""' WHERE feedback='"""+self.name+"""'""")
