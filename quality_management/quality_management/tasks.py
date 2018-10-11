269276b40274e1868a258cee6a40920bb4cce484
Anurag mishra <anuragmishra@Anurags-MacBook-Air.local>
2018-10-11 15:33:51 +0530

    changes schdulers

--- quality_management/quality_management/tasks.py
+++ quality_management/quality_management/tasks.py
@@ -2,13 +2,13 @@ from __future__ import unicode_literals
 import frappe
 from frappe.utils import date_diff, nowdate, formatdate, add_days
 
-def all():
-	doc = frappe.get_doc({
-		'doctype': 'Quality Action',
-		'action': 'Corrective',
-		'type': 'Quality Review',
-		'date': ''+ frappe.utils.nowdate() +'',
-		'problem': 'hello'
-	})
-	doc.insert()
-	doc.name
+#def all():
+#	doc = frappe.get_doc({
+#		'doctype': 'Quality Action',
+#		'action': 'Corrective',
+#		'type': 'Quality Review',
+#		'date': ''+ frappe.utils.nowdate() +'',
+#		'problem': 'hello'
+#	})
+#	doc.insert()
+#	doc.name
