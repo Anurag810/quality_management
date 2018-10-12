7b5b852828aab51c97472749116c383159bb5bc9
Himanshu Warekar <himanshuwarekar@Himanshus-MacBook-Air.local>
2018-10-12 12:17:36 +0530

    changes

--- quality_management/quality_management/tasks.py
+++ quality_management/quality_management/tasks.py
@@ -2,12 +2,20 @@ from __future__ import unicode_literals
 import frappe
 from frappe.utils import date_diff, nowdate, formatdate, add_days
 
-def create_audit():
+def audit():
 	doc = frappe.get_doc({
 		'doctype': 'Quality Audit',
 		'from_date': ''+ frappe.utils.nowdate() +'',
         'status':'Scheduled',
         'action':'Under Review'
+	})
+	doc.insert()
+	doc.name
+
+def review():
+    	doc = frappe.get_doc({
+		'doctype': 'Quality Review',
+		
 	})
 	doc.insert()
 	doc.name
