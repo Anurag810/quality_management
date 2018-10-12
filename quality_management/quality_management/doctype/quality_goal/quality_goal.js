03332799461b67737fec043f58a6d5288bb935c6
Anurag mishra <anuragmishra@Anurags-MacBook-Air.local>
2018-10-12 14:18:37 +0530

    changes

--- quality_management/quality_management/doctype/quality_goal/quality_goal.js
+++ quality_management/quality_management/doctype/quality_goal/quality_goal.js
@@ -10,10 +10,25 @@ frappe.ui.form.on('Quality Goal', {
 	},
 	frequency: function(frm){
 		var a = frm.doc.frequency;
-		var n = a.indexOf("Daily");
-		if (n != -1){
+		if (a.indexOf("Daily") != -1){
 			 frm.set_value("next_date", frappe.datetime.add_days(frappe.datetime.get_today(), 1));
-}
+		}
+		else if(a.indexOf("Weekly") != -1){
+			alert("Weekly");
+		}
+		else if(a.indexOf("Monthly") != -1){
+			alert("Monthly");
+		}
+		else if(a.indexOf("Quarterly") != -1){
+			alert("Q")
+		}
+		else if(a.indexOf("Half") != -1){
+			alert("Half");
+		}
+		else if(a.indexOf("Yearly") != -1){
+			alert("Yearly");
+		}
+
 	}
 });
 
