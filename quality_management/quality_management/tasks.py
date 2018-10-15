07620d4b9a0655887c5cce87d746efcb5e30ae4c
Himanshu Warekar <himanshuwarekar@Himanshus-MacBook-Air.local>
2018-10-15 12:35:13 +0530

    task scheduling

--- quality_management/quality_management/tasks.py
+++ quality_management/quality_management/tasks.py
@@ -19,44 +19,42 @@ def review():
 	day_name = now.strftime("%A")
 	month=now.strftime("%B")
 
+	def get_objective(name):
+		objectives = frappe.get_all("Quality Objective", filters={'parent': ''+ name +''}, fields=['objective', 'target', 'unit'])
+		return objectives
+
 	for data in frappe.get_all("Quality Goal",fields=['name','scheduler','frequency']):
-		print("----"+data.name)
-		print("--------"+data.scheduler)
-		print("--------"+data.frequency)
 		if data.frequency.find("Everyday") > -1:
-			pass
+			print("EVERYDAY----"+ data.name)
+			objectives = get_objective(data.name)
+			print(objectives)
+
 		elif data.frequency.find("Weekly") > -1:
 			if data.scheduler == day_name:
-				pass
+				print("WEEKLY----"+ data.name)
+				objectives = get_objective(data.name)
+				print(objectives)
+
 		elif data.frequency.find("Monthly") > -1:
 			if data.scheduler == str(day):
-				pass
+				print("MONTHLY----"+ data.name)
+				objectives = get_objective(data.name)
+				print(objectives)
+
 		elif data.frequency.find("Quarterly") > -1:
-			if month == 'January' and day == 1:
-				pass
-			elif month == 'April' and day == 1:
-				pass
-			elif month == 'July' and day == 1:
-				pass
-			elif month == 'October' and day == 1:
-				pass
+			if (month == 'January' or month == 'April' or month == 'July' or month == 'October') and day == 1:
+				print("QUARTERLY----"+ data.name)
+				objectives = get_objective(data.name)
+				print(objectives)
+
 		elif data.frequency.find("Half") > -1:
-			if month == 'January' and day == 1:
-				pass
-			elif month == 'July' and day == 1:
-				pass
+			if (month == 'January' or month == 'July') and day == 1:
+				print("HALF----"+ data.name)
+				objectives = get_objective(data.name)
+				print(objectives)
+
 		elif data.frequency.find("Yearly") > -1:
 			if month == 'January' and day == 1:
-				pass
-
-#	data = frappe.get_doc('Quality Goal')
-#	goals = [frappe.get_doc('Quality Goal', d.name) for d in data]
-#	objectives = [[d.objective for d in goal.objective] for goal in goals]
-#	targets = [[d.target for d in goal.objective] for goal in goals]
-#	units = [[d.unit for d in goal.objective] for goal in goals]
-
-#	doc = frappe.get_doc({
-#		'doctype': 'Quality Review',
-#	})
-#	doc.insert()
-#	doc.name
+				print("YEARLY----"+ data.name)
+				objectives = get_objective(data.name)
+				print(objectives)
