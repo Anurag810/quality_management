05b9174721a17ab2561e5f36c21e9ca0abb5f71d
Himanshu Warekar <himanshuwarekar@Himanshus-MacBook-Air.local>
2018-10-15 16:05:38 +0530

    feedback template

--- quality_management/config/quality_management.py
+++ quality_management/config/quality_management.py
@@ -56,6 +56,11 @@ def get_data():
                     "type": "doctype",
                     "name": "Customer Feedback"
 
+                },
+                {
+                    "type": "doctype",
+                    "name": "Feedback Template"
+
                 }
             ]
         }
--- quality_management/quality_management/doctype/customer_feedback/customer_feedback.js
+++ quality_management/quality_management/doctype/customer_feedback/customer_feedback.js
@@ -5,7 +5,27 @@ frappe.ui.form.on('Customer Feedback', {
 	refresh: function(frm) {
 
 	},
-	customer: function(frm){
+	onload: function(frm){
 		frm.set_value("date", frappe.datetime.get_today())
+	},
+	customer: function(frm){
+		
+	},
+	template: function(frm){
+		frappe.call({
+            "method": "frappe.client.get",
+            args: {
+                doctype: "Feedback Template",
+				name: frm.doc.template
+            },
+            callback: function (data) {
+				console.log(data.message);
+				for (var i = 0; i < data.message.feedback_parameter.length; i++ ){
+					frm.add_child("feedback");
+					frm.fields_dict.feedback.get_value()[i].parameter = data.message.feedback_parameter[i].parameter;
+				}
+				frm.refresh();
+            }
+        })
 	}
 });
