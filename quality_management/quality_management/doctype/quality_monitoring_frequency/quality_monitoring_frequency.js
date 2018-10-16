800b9ee8dfc5bac64746e1b8862f50cba79451bf
Himanshu Warekar <himanshuwarekar@Himanshus-MacBook-Air.local>
2018-10-16 11:36:45 +0530

    code refactor

--- quality_management/quality_management/doctype/quality_monitoring_frequency/quality_monitoring_frequency.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2018, Frappe and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Quality Monitoring Frequency', {
-	onload: function(frm) {
-
-	},
-});
