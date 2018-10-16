fb4287f9859838571b869180c52b96b7cd34c697
Anurag mishra <anuragmishra@Anurags-MacBook-Air.local>
2018-10-16 18:12:46 +0530

    while working on list view

---
+++ quality_management/quality_management/doctype/quality_action/quality_action_list.js
@@ -0,0 +1,15 @@
+frappe.listview_settings['Quality Action'] = {
+	add_fields: ["status"],
+	get_indicator: function(doc) {
+		if(doc.status == "Planned") {
+			return [__("Planned"), "yellow", "status,=,Planned"];
+		}
+		else if(doc.status == "Under Review") {
+			return [__("Under Review"), "green", "status,=,Under Review"];
+		}
+		else if(doc.status == "Patched") {
+			return [__("Patched"), "blue", "status,=,Patched"];
+		}
+		
+	}	
+}
