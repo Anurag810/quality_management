8a9b3e8aca7438465d7f1142fecf3daf34c21f58
Himanshu Warekar <himanshuwarekar@Himanshus-MacBook-Air.local>
2018-10-16 12:52:30 +0530

    bug fix

--- quality_management/quality_management/doctype/quality_action/quality_action.js
+++ quality_management/quality_management/doctype/quality_action/quality_action.js
@@ -31,7 +31,7 @@ frappe.ui.form.on('Quality Action', {
 						problems += data.message.values[i].objective +"-"+ data.message.values[i].achieved + " " + data.message.values[i].target_unit + "\n";
 					}
 				}
-				problems = problems.slice(0, -2);
+				problems = problems.replace(/\n$/, "");
 				problems= problems.split("\n");
 				for (var i = 0; i < problems.length; i++){
 					frm.add_child("description");
