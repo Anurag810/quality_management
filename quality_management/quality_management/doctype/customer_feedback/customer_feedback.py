f303dbe87d722ad31444e8cb204394e93bd69a2b
Himanshu Warekar <himanshuwarekar@Himanshus-MacBook-Air.local>
2018-10-17 12:57:22 +0530

    bug fix

--- quality_management/quality_management/doctype/customer_feedback/customer_feedback.py
+++ quality_management/quality_management/doctype/customer_feedback/customer_feedback.py
@@ -9,24 +9,38 @@ from frappe.model.document import Document
 class CustomerFeedback(Document):
 	
 	def validate(self):
+		rating = 0
+		total = 5 * len(self.feedback)
 		for data in self.feedback:
-			print(data.qualitative_feedback)
+			rating += int(data.rating)
 
-	def create_action(self):
-		query = frappe.get_list("Quality Action", filters={"feedback": ""+ self.name +""})
-		if len(query) == 0:
-			doc = frappe.get_doc({
-				'doctype': 'Quality Action',
-				'action': 'Corrective',
-				'type': 'Customer Feedback',
-				'feedback': ''+ self.name +'',
-				'date': ''+ frappe.utils.nowdate() +''
-			})
-			for data in self.feedback:
-				print(data.qualitative_feedback)
-				doc.append("description",{
-					'problem': data.qualitative_feedback,
-					'status': 'Opened'
+		if (rating*100)/total < 75:
+			self.action = "Action Initialised"
+		
+
+	def after_insert(self):
+		rating = 0
+		total = 5 * len(self.feedback)
+		for data in self.feedback:
+			rating += int(data.rating)
+
+		if (rating*100)/total < 75:
+			query = frappe.get_list("Quality Action", filters={"feedback": ""+ self.name +""})
+			if len(query) == 0:
+				doc = frappe.get_doc({
+					'doctype': 'Quality Action',
+					'action': 'Corrective',
+					'type': 'Customer Feedback',
+					'feedback': ''+ self.name +'',
+					'date': ''+ frappe.utils.nowdate() +''
 				})
-			doc.insert()
-			frappe.db.commit()
+				for data in self.feedback:
+					print(data.qualitative_feedback)
+					doc.append("description",{
+						'problem': data.qualitative_feedback,
+						'status': 'Opened'
+					})
+				doc.insert()
+				frappe.db.commit()
+		else:
+			pass
