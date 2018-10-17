343810e1757ec01db2cd3c86b8becaed5527ffd1
Anurag mishra <anuragmishra@Anurags-MacBook-Air.local>
2018-10-17 12:33:32 +0530

    list view

--- quality_management/quality_management/doctype/quality_review/quality_review_list.js
+++ quality_management/quality_management/doctype/quality_review/quality_review_list.js
@@ -2,10 +2,10 @@ frappe.listview_settings['Quality Review'] = {
 	add_fields: ["action"],
 	get_indicator: function(doc) {
 		if(doc.action == "No Action") {
-			return [__("No Action"), "blue", "action,=,No Action"];
+			return [__("No Action"), "green", "action,=,No Action"];
 		}
 		else if(doc.action == "Action Initialised") {
-			return [__("Action Initialised"), "green", "action,=,Action Initialised"];
+			return [__("Action Initialised"), "red", "action,=,Action Initialised"];
 		}		
 	}	
 }
