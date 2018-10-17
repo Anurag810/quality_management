5e0861fb7d77d401e9db5a8711fac2137d474dbc
Anurag mishra <anuragmishra@Anurags-MacBook-Air.local>
2018-10-17 14:48:22 +0530

    changes

--- quality_management/quality_management/doctype/quality_action/quality_action_list.js
+++ quality_management/quality_management/doctype/quality_action/quality_action_list.js
@@ -3,10 +3,10 @@ frappe.listview_settings['Quality Action'] = {
 	add_fields: ["action"],
 	get_indicator: function(doc) {
 		if(doc.status == "Planned") {
-			return [__("Planned"), "yellow", "status,=,Planned"];
+			return [__("Planned"), "green", "status,=,Planned"];
 		}
 		else if(doc.status == "Under Review") {
-			return [__("Under Review"), "green", "status,=,Under Review"];
+			return [__("Under Review"), "red", "status,=,Under Review"];
 		}
 		else if(doc.status == "Patched") {
 			return [__("Patched"), "blue", "status,=,Patched"];	
